84ab2ef9bc876fbc5475b896cbc9e57fe7de55c6
Kyle Geske <stungeye@gmail.com>
2019-12-11 16:22:14 -0600

    Reverse string challenge using <algorithm> reverse.

--- cpp/reverse-string/reverse_string.cpp
+++ cpp/reverse-string/reverse_string.cpp
@@ -1,5 +1,12 @@
 #include "reverse_string.h"
+#include <algorithm>
 
 namespace reverse_string {
+    string reverse_string(const string& input) {
+        string output(input);
 
+        reverse(output.begin(), output.end());
+
+        return output;
+    }
 }  // namespace reverse_string
