7e1b988ca7f1c6696049c6ac8410e01efed8bc7f
Kyle Geske <stungeye@gmail.com>
2019-12-10 20:31:58 -0600

    Completed the CPP Hello World.

--- cpp/hello-world/hello_world.cpp
+++ cpp/hello-world/hello_world.cpp
@@ -10,7 +10,7 @@ namespace hello_world {
 // std::string hello_world::hello()
 string hello() {
     // Return the string we need.
-    return "Fix me!";
+    return "Hello, World!";
 }
 
 }  // namespace hello_world
