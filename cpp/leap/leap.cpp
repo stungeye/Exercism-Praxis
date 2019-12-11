35b8847330497c84745218d87151930389ac8515
Kyle Geske <stungeye@gmail.com>
2019-12-11 16:02:47 -0600

    Refactored leap to be more readable.

--- cpp/leap/leap.cpp
+++ cpp/leap/leap.cpp
@@ -2,10 +2,15 @@
 
 namespace leap
 {
-    bool is_leap_year(int year)
-    {
-        return year % 100 == 0
-                   ? year % 400 == 0
-                   : year % 4 == 0;
-    }
+	bool evenly_divisible_by(const int value, const int divisor)
+	{
+		return value % divisor == 0;
+	}
+
+	bool is_leap_year(const int year)
+	{
+		return evenly_divisible_by(year, 4)
+			&& (evenly_divisible_by(year, 400)
+				|| !evenly_divisible_by(year, 100));
+	}
 } // namespace leap
