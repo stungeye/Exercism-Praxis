4f72a24a7d25d4ed1f0dea762693f10bfd4c9a31
Kyle Geske <stungeye@gmail.com>
2019-12-11 16:21:52 -0600

    Tabs vs spaces?

--- cpp/hamming/hamming.cpp
+++ cpp/hamming/hamming.cpp
@@ -1,18 +1,18 @@
 #include "hamming.h"
 
 namespace hamming {
-	int compute(const string& strand1, const string& strand2) {
-		if (strand1.length() != strand2.length()) {
-			throw domain_error("Strand lengths do not match.");
-		}
+    int compute(const string& strand1, const string& strand2) {
+        if (strand1.length() != strand2.length()) {
+            throw domain_error("Strand lengths do not match.");
+        }
 
-		vector<bool> matches;
+        vector<bool> matches;
 
-		transform(strand1.begin(), strand1.end(),
-		          strand2.begin(),
-		          std::back_inserter(matches),
-		          [](char c1, char c2) { return c1 == c2; });
+        transform(strand1.begin(), strand1.end(),
+                  strand2.begin(),
+                  std::back_inserter(matches),
+                  [](char c1, char c2) { return c1 == c2; });
 
-		return count(matches.begin(), matches.end(), false);
-	}
+        return count(matches.begin(), matches.end(), false);
+    }
 } // namespace hamming
