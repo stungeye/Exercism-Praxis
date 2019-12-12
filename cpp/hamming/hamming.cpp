7b397a881d57dd98ed5503fa246a5831977607a3
Kyle Geske <stungeye@gmail.com>
2019-12-12 13:22:03 -0600

    Moving to 2 space tabs.

--- cpp/hamming/hamming.cpp
+++ cpp/hamming/hamming.cpp
@@ -1,15 +1,15 @@
 #include "hamming.h"
-#include <numeric>
 #include <functional>
+#include <numeric>
 
 namespace hamming {
-    int compute(const std::string& strand1, const std::string& strand2) {
-        if (strand1.length() != strand2.length()) {
-            throw std::domain_error("Strand lengths do not match.");
-        }
-
-        return std::inner_product(strand1.begin(), strand1.end(), strand2.begin(),
-            0, std::plus<>(), std::not_equal_to<>()
-        );
+  int compute(const std::string& strand1, const std::string& strand2) {
+    if (strand1.length() != strand2.length()) {
+      throw std::domain_error("Strand lengths do not match.");
     }
-}
+
+    return std::inner_product(strand1.begin(), strand1.end(), strand2.begin(),
+                              0, std::plus<>(), std::not_equal_to<>()
+    );
+  }
+} // namespace hamming
--- cpp/hamming/hamming.h
+++ cpp/hamming/hamming.h
@@ -1,11 +1,11 @@
 #if !defined(HAMMING_H)
 #define HAMMING_H
 
-#include <string>
 #include <stdexcept>
+#include <string>
 
 namespace hamming {
-	int compute(const std::string& strand1, const std::string& strand2);
-}  // namespace hamming
+  int compute(const std::string& strand1, const std::string& strand2);
+} // namespace hamming
 
 #endif // HAMMING_H
