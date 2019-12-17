59ba8d8a049b4872ceafd764809bd57554fe7e8c
Kyle Geske <stungeye@gmail.com>
2019-12-17 07:24:44 -0600

    Solved the Two Fer Challenge.

---
+++ cpp/two-fer/two_fer.cpp
@@ -0,0 +1,14 @@
+#include "two_fer.h"
+
+namespace two_fer
+{
+
+  std::string two_fer() {
+    return "One for you, one for me.";
+  }
+
+  std::string two_fer(std::string name) {
+    return "One for " + name + ", one for me.";
+  }
+} // namespace two_fer
+
