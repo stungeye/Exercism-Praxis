5b0127081336395547179f45b47438462fa37957
Kyle Geske <stungeye@gmail.com>
2019-12-17 07:13:53 -0600

    Completed the Grade School challenge.

--- cpp/grade-school/grade_school.cpp
+++ cpp/grade-school/grade_school.cpp
@@ -1,5 +1,22 @@
 #include "grade_school.h"
 
-namespace grade_school {
+#include <vector>
+#include <algorithm>
 
+namespace grade_school {
+  void school::add(const std::string& name, int grade) {
+    grades[grade].push_back(name);
+    sort(grades[grade].begin(), grades[grade].end());
+  }
+
+  std::map<int, std::vector<std::string>> school::roster() const {
+    return grades;
+  }
+
+  std::vector<std::string> school::grade(int grade_number) const {
+    if (grades.count(grade_number) == 0) {
+      return std::vector<std::string>();
+    }
+    return grades.at(grade_number);
+  }
 }  // namespace grade_school
--- cpp/grade-school/grade_school.h
+++ cpp/grade-school/grade_school.h
@@ -1,8 +1,18 @@
 #if !defined(GRADE_SCHOOL_H)
 #define GRADE_SCHOOL_H
 
-namespace grade_school {
+#include <map>
+#include <string>
+#include <vector>
 
+namespace grade_school {
+  class school {
+    std::map<int, std::vector<std::string>> grades {};
+  public:
+    void add(const std::string& name, int grade);
+    std::map<int, std::vector<std::string>> roster() const;
+    std::vector<std::string> grade(int grade_number) const;
+  };
 }  // namespace grade_school
 
 #endif // GRADE_SCHOOL_H
