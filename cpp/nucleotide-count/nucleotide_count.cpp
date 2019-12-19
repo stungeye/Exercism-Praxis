4e2ef704627bcc345628f8d564556d324eafd4a7
Kyle Geske <stungeye@gmail.com>
2019-12-19 10:46:12 -0600

    Finished refactoring Nucleotide Count challenge.

--- cpp/nucleotide-count/nucleotide_count.cpp
+++ cpp/nucleotide-count/nucleotide_count.cpp
@@ -3,37 +3,35 @@
 #include <algorithm>
 #include <stdexcept>
 
-
 namespace nucleotide_count {
-    counter::counter(const std::string& sequence) {
-        if (std::count_if(sequence.begin(), sequence.end(),
-                          [](char n) { return invalid_nucleotide(n); }) != 0) {
-            throw std::invalid_argument("Sequence contains an invalid nucleotide.");
-        }
-        assemble_nucleotide_counts(sequence);
-    }
+	counter::counter(const std::string& sequence) {
+		if (std::any_of(sequence.begin(), sequence.end(), invalid_nucleotide)) {
+			throw std::invalid_argument("Sequence contains an invalid nucleotide.");
+		}
+		assemble_nucleotide_counts(sequence);
+	}
 
-    std::map<char, int> counter::nucleotide_counts() const {
-        return counts;
-    }
+	std::map<char, int> counter::nucleotide_counts() const {
+		return counts;
+	}
 
-    int counter::count(char nucleotide) const {
-        if (invalid_nucleotide(nucleotide)) {
-            throw std::invalid_argument("Count requested for invalid nucleotide.");
-        }
-        return counts.at(nucleotide);
-    }
+	int counter::count(char nucleotide) const {
+		if (invalid_nucleotide(nucleotide)) {
+			throw std::invalid_argument("Count requested for invalid nucleotide.");
+		}
+		return counts.at(nucleotide);
+	}
 
-    // Private Methods
+	// Private Methods
 
-    bool counter::invalid_nucleotide(char nucleotide) {
-        return possible_nucleotides.find(nucleotide) == std::string::npos;
-    }
+	bool counter::invalid_nucleotide(char nucleotide) {
+		return possible_nucleotides.find(nucleotide) == std::string::npos;
+	}
 
-    void counter::assemble_nucleotide_counts(const std::string& sequence) {
-        for (auto nucleotide : possible_nucleotides) {
-            counts[nucleotide] = std::count(sequence.begin(), sequence.end(), nucleotide);
-        }
-    }
+	void counter::assemble_nucleotide_counts(const std::string& sequence) {
+		for (auto nucleotide : possible_nucleotides) {
+			counts[nucleotide] = static_cast<int>(std::count(sequence.begin(), sequence.end(), nucleotide));
+		}
+	}
 
 } // namespace nucleotide_count
--- cpp/nucleotide-count/nucleotide_count.h
+++ cpp/nucleotide-count/nucleotide_count.h
@@ -5,17 +5,17 @@
 #include <string>
 
 namespace nucleotide_count {
-    class counter {
-        inline static const std::string possible_nucleotides{"ATCG"};
-        std::map<char, int> counts;
-    public:
-        explicit counter(const std::string& sequence);
-        std::map<char, int> nucleotide_counts() const;
-        int count(char nucleotide) const;
-    private:
-        static bool invalid_nucleotide(char nucleotide);
-        void assemble_nucleotide_counts(const std::string& sequence);
-    };
+	class counter {
+	    inline static const std::string possible_nucleotides{"ATCG"};
+		std::map<char, int> counts;
+	public:
+		explicit counter(const std::string& sequence);
+		[[nodiscard]] std::map<char, int> nucleotide_counts() const;
+		[[nodiscard]] int count(char nucleotide) const;
+	private:
+		static bool invalid_nucleotide(char nucleotide);
+		void assemble_nucleotide_counts(const std::string& sequence);
+	};
 
 } // namespace nucleotide_count
 
