c7da5d00f8a5a7ff77814392a1940fef3d43b0d9
Kyle Geske <stungeye@gmail.com>
2019-12-27 09:45:52 -0600

    One final pangram refactor.

--- cpp/pangram/pangram.cpp
+++ cpp/pangram/pangram.cpp
@@ -4,10 +4,10 @@
 
 namespace pangram {
     // ASCII table positions for Roman letters.
-    inline const char ascii_a = 97;
-    inline const char ascii_z = 122;
-    inline const char ascii_A = 65;
-    inline const char ascii_Z = 90;
+    inline const char a = 'a';
+    inline const char z = 'z';
+    inline const char A = 'A';
+    inline const char Z = 'Z';
 
     inline const int letters_in_alphabet = 26;
 
@@ -19,12 +19,12 @@ namespace pangram {
         // Letters tracked by their ASCII table position.
         // i.e. letters_seen[0] represents both 'A' and 'a'.
         for (auto c : sentence) {
-            if ((c >= ascii_A) && (c <= ascii_Z )) {
-                letters_seen.set(c - ascii_A);
+            if ((c >= A) && (c <= Z )) {
+                letters_seen.set(c - A);
             }
 
-            if ((c >= ascii_a) && (c <= ascii_z)) {
-                letters_seen.set(c - ascii_a);
+            if ((c >= a) && (c <= z)) {
+                letters_seen.set(c - a);
             }
         }
 
