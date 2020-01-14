774e813ca5071bd443ec735321ab6aaf17a04f87
Kyle Geske <stungeye@gmail.com>
2020-01-14 15:43:57 -0600

    First test is passing.

--- cpp/sum-of-multiples/sum_of_multiples.cpp
+++ cpp/sum-of-multiples/sum_of_multiples.cpp
@@ -2,4 +2,7 @@
 
 namespace sum_of_multiples {
 
+	int to(const std::vector<int>& factors, int exclusive_upper_bound) {
+		return 0;
+	}
 }  // namespace sum_of_multiples
--- cpp/sum-of-multiples/sum_of_multiples.h
+++ cpp/sum-of-multiples/sum_of_multiples.h
@@ -1,8 +1,10 @@
 #if !defined(SUM_OF_MULTIPLES_H)
 #define SUM_OF_MULTIPLES_H
 
-namespace sum_of_multiples {
+#include <vector>
 
+namespace sum_of_multiples {
+	int to(const std::vector<int>& factors, int exclusive_upper_bound);
 }  // namespace sum_of_multiples
 
 #endif // SUM_OF_MULTIPLES_H
