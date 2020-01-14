0e5628d8e3085785448ab4052298aa1990ae9eb5
Kyle Geske <stungeye@gmail.com>
2020-01-13 21:27:33 -0600

    Namespace aliases for ETL.

--- cpp/etl/etl.cpp
+++ cpp/etl/etl.cpp
@@ -4,9 +4,9 @@
 
 namespace etl {
 
-    std::map<char, int> transform(const std::map<int, std::vector<char>>& old_points) {
-        std::map<char, int> output;
-        
+    modern_score_map transform(const legacy_score_map& old_points) {
+        modern_score_map output;
+
         for (const auto& value_to_letters : old_points) {
             const auto value = value_to_letters.first;
             const auto letters = value_to_letters.second;
--- cpp/etl/etl.h
+++ cpp/etl/etl.h
@@ -5,7 +5,11 @@
 #include <map>
 
 namespace etl {
-    std::map<char, int> transform(const std::map<int, std::vector<char>>& old_points);
+
+    using legacy_score_map = std::map<int, std::vector<char>>;
+    using modern_score_map = std::map<char, int>;
+
+    modern_score_map transform(const legacy_score_map& old_points);
 }  // namespace etl
 
 #endif // ETL_H
