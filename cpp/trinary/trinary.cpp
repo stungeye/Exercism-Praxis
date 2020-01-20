e99344aa7380cb2addf32d14f06ef09b79e2319a
Kyle Geske <stungeye@gmail.com>
2020-01-20 11:42:56 -0600

    Refactored the trinary challenge.

--- cpp/trinary/trinary.cpp
+++ cpp/trinary/trinary.cpp
@@ -2,18 +2,17 @@
 
 namespace trinary {
 	int from_base_to_decimal(const int base, const std::string& numerals) {
-		const auto length = numerals.length();
 		auto decimal = 0;
 
-		for (size_t i = 0; i < length; ++i) {
-			const size_t position_from_end = length - 1 - i;
-			const auto digit = numerals.at(position_from_end) - '0';
+		for (const auto numeral : numerals) {
+			const auto digit = numeral - '0';
 
 			if ((digit < 0) || (digit >= base)) {
 				return 0;
 			}
 
-			decimal += digit * pow(base, i);
+			decimal *= base;
+			decimal += digit;
 		}
 
 		return decimal;
