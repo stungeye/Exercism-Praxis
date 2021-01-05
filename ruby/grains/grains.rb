214c64206cfff04ee51d6d7858eac7f43b6279d0
Kyle Geske <stungeye@gmail.com>
2021-01-05 13:19:47 -0600

    Added shameless mathy solution. :)

--- ruby/grains/grains.rb
+++ ruby/grains/grains.rb
@@ -1,13 +1,28 @@
+# Naive code-based implementation:
+
 class Grains
-  def self.square( position )
-    chessboard[ position - 1 ]
+  def self.chessboard
+    (0..63).map { |position| 2**position }
   end
 
-  def self.chessboard
-    ( 0..63 ).map { |position| 2 ** position }
+  def self.square(position)
+    chessboard[position - 1]
   end
 
   def self.total
-    chessboard.reduce( :+ )
+    chessboard.reduce(:+)
+  end
+end
+
+# Math for the win:
+
+class Grains
+  def self.square(position)
+    2**(position - 1)
+  end
+
+  def self.total
+    # Shameless green! Chessboards are always the same size. :)
+    18_446_744_073_709_551_615
   end
 end
