6476dfea02e0879a9a390932726d9bd38f131916
Kyle Geske <stungeye@gmail.com>
2022-02-23 07:47:50 -0600

    Completed C++ Collatz Conjecture

--- cpp/collatz-conjecture/collatz_conjecture.cpp
+++ cpp/collatz-conjecture/collatz_conjecture.cpp
@@ -1,5 +1,38 @@
 #include "collatz_conjecture.h"
 
-namespace collatz_conjecture {
+#include <stdexcept>
 
+namespace collatz_conjecture {
+	// Recursive Solution
+	int steps(int n) {
+		if (n <= 0) throw std::domain_error("Argument must be greater than zero.");
+
+		// Base case: No more steps when n is 1.
+		if (n == 1) return 0; 
+
+		// Otherwise: calculate the next value of n and record one step.
+		const int nextN = (n % 2) ? (3 * n + 1) : (n / 2);
+		return 1 + steps(nextN);
+	}
+
+/*
+	// Iterative Solution
+	int steps(int n) {
+		if (n <= 0) throw std::domain_error("Argument must be greater than zero.");
+
+		int stepCount = 0;
+
+		while(n != 1) {
+			if (n % 2 == 0) {
+				n /= 2;
+			} else {
+				n = n * 3 + 1;
+			}
+
+			++stepCount;
+		}
+
+		return stepCount;
+	}
+*/
 }  // namespace collatz_conjecture
--- cpp/collatz-conjecture/collatz_conjecture.h
+++ cpp/collatz-conjecture/collatz_conjecture.h
@@ -2,7 +2,7 @@
 #define COLLATZ_CONJECTURE_H
 
 namespace collatz_conjecture {
-
+	int steps(int n);
 }  // namespace collatz_conjecture
 
 #endif // COLLATZ_CONJECTURE_H
