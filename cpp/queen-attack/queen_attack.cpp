d6728beff899f37900b8257e6a5bfa7dc81c875f
Kyle Geske <stungeye@gmail.com>
2022-02-28 14:49:28 -0600

    Completed queen attack.

--- cpp/queen-attack/queen_attack.cpp
+++ cpp/queen-attack/queen_attack.cpp
@@ -1,5 +1,36 @@
 #include "queen_attack.h"
 
-namespace queen_attack {
+#include <iostream>
+#include <stdexcept>
 
+
+namespace queen_attack {
+	chess_board::chess_board(position white, position black) : m_white{white}, m_black{black} {
+		if (white == black) throw std::domain_error("Black and white positions cannot be the same.");
+	}
+
+	position chess_board::white() const {
+		return m_white;
+	}
+
+	position chess_board::black() const {
+		return m_black;
+	}
+
+	bool chess_board::can_attack() const {
+		return m_white.first == m_black.first
+			   || m_white.second == m_black.second
+			   || std::abs(m_white.first - m_black.first)
+		            == std::abs(m_white.second - m_black.second);
+	}
+
+	chess_board::operator std::string() const {
+		const std::string row{ "_ _ _ _ _ _ _ _\n" };
+		std::string board{ row + row + row + row + row + row + row + row };
+
+		board[m_white.first * 16 + m_white.second * 2] = 'W';
+		board[m_black.first * 16 + m_black.second * 2] = 'B';
+
+		return board;
+	}
 }  // namespace queen_attack
--- cpp/queen-attack/queen_attack.h
+++ cpp/queen-attack/queen_attack.h
@@ -1,7 +1,26 @@
 #if !defined(QUEEN_ATTACK_H)
 #define QUEEN_ATTACK_H
 
+#include <string>
+#include <utility>
+
 namespace queen_attack {
+
+	using position = std::pair<int, int>;
+
+	class chess_board {
+		position m_white{ 0, 3 };
+		position m_black{ 7, 3 };
+
+	public:
+		chess_board() = default;
+		chess_board(position white, position black);
+		position white() const;
+		position black() const;
+		bool can_attack() const;
+
+		operator std::string() const;
+	};
 
 }  // namespace queen_attack
 
