d3dcab8900163b3a46dce7a4ff4e0c516ea759e1
Kyle Geske <stungeye@gmail.com>
2022-03-28 13:14:10 -0500

    Completed the app allergies cpp challenge.

--- cpp/allergies/allergies.cpp
+++ cpp/allergies/allergies.cpp
@@ -1,5 +1,32 @@
 #include "allergies.h"
 
-namespace allergies {
+#include <vector>    // std::vector
+#include <algorithm> // std::copy_if
+#include <cmath>     // std::pow
+#include <iterator>  // std::inserter
 
-}  // namespace allergies
+namespace allergies {
+	const std::vector<std::string> allergens{
+		"eggs", "peanuts", "shellfish", "strawberries", "tomatoes", "chocolate", "pollen", "cats"
+	};
+
+	allergy_test::allergy_test(const int allergy_code) {
+		// Lambda made "mutable" to allow index to increase with each call.
+		// [[maybe_unused]] added to suppress warnings on Exericism as the parameter is not used.
+		std::copy_if(allergens.begin(), allergens.end(),
+		             std::inserter(allergies, allergies.begin()),
+		             [allergy_code, index = 0]([[maybe_unused]] const auto& allergen) mutable -> bool {
+			             return allergy_code & static_cast<int>(std::pow(2, index++));
+		             });
+
+	}
+
+	bool allergy_test::is_allergic_to(const std::string& potential_allergen) const {
+		return allergies.count(potential_allergen) == 1;
+	}
+
+	std::unordered_set<std::string> allergy_test::get_allergies() const {
+		return allergies;
+	}
+
+} // namespace allergies
--- cpp/allergies/allergies.h
+++ cpp/allergies/allergies.h
@@ -1,7 +1,17 @@
 #if !defined(ALLERGIES_H)
 #define ALLERGIES_H
 
+#include <string>
+#include <unordered_set>
+
 namespace allergies {
+	class allergy_test {
+		std::unordered_set<std::string> allergies;
+	public:
+		allergy_test(const int allergy_code);
+		[[nodiscard]] bool is_allergic_to(const std::string& potential_allergen) const;
+		[[nodiscard]] std::unordered_set<std::string> get_allergies() const;
+	};
 
 }  // namespace allergies
 
