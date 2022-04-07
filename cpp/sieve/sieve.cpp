2172d8e5584d8178f5a3017839d988c4726992b5
Kyle Geske <stungeye@gmail.com>
2022-04-07 09:51:10 -0500

    Added a slightly more efficient version of C++20 Sieve.

--- cpp/sieve/sieve.cpp
+++ cpp/sieve/sieve.cpp
@@ -1,9 +1,10 @@
 #include "sieve.h"
 
+#include <algorithm> // std::ranges::generate
 #include <numeric>   // std::iota
 
 namespace sieve {
-	constexpr int smallestPrime{ 2 };
+	constexpr int smallestPrime{2};
 
 	std::vector<int> primes(const int limit) {
 		if (limit < smallestPrime) return {};
@@ -12,11 +13,32 @@ namespace sieve {
 		std::vector<int> potentialPrimes(limit - 1);
 		std::iota(potentialPrimes.begin(), potentialPrimes.end(), smallestPrime);
 
+		// Remove multiples of each prime number from potential candidates.
 		for (auto prime : potentialPrimes) {
 			std::erase_if(potentialPrimes, [prime](auto num) {
 				return prime != num && num % prime == 0;
 				});
 		}
+
+		return potentialPrimes;
+	}
+
+	std::vector<int> primesOptimized(const int limit) {
+		if (limit < smallestPrime) return {};
+
+		// Fill potentialPrimes with odd integers from 3 to limit.
+		std::vector<int> potentialPrimes((limit - 1) / 2);
+		std::ranges::generate(potentialPrimes, [n = smallestPrime - 1]() mutable { n += 2;  return n; });
+
+		// Remove multiples of each prime number from potential candidates.
+		for (auto prime : potentialPrimes) {
+			std::erase_if(potentialPrimes, [prime](auto num) {
+				return prime != num && num % prime == 0;
+			});
+		}
+
+		// 2 is the only even prime, so you add it to the front manually.
+		potentialPrimes.insert(potentialPrimes.begin(), smallestPrime);
 
 		return potentialPrimes;
 	}
