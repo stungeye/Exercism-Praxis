5f1619781944f346def0896fbf932de2fce8df8b
Kyle Geske <stungeye@gmail.com>
2022-04-07 08:44:51 -0500

    Solved C++ Sieve using Cpp17.

--- cpp/sieve/sieve.cpp
+++ cpp/sieve/sieve.cpp
@@ -1,5 +1,43 @@
 #include "sieve.h"
 
+#include <algorithm>
+#include <cmath>
+
 namespace sieve {
 
-}  // namespace sieve
+	constexpr int smallestPrime{2};
+
+	std::vector<int> sequence(int startingValue, int endingValue) {
+		std::vector<int> v(endingValue - startingValue + 1);
+		std::generate(v.begin(), v.end(),
+		              [n = startingValue]() mutable { return n++; });
+		return v;
+	}
+
+	bool includes(std::vector<int> haystack, int needle) {
+		const auto it{std::find(haystack.begin(), haystack.end(), needle)};
+		return it != haystack.end();
+	}
+
+	std::vector<int> primes(int limit) {
+		if (limit < smallestPrime) return {};
+
+		const auto testRange{sequence(smallestPrime, static_cast<int>(std::sqrt(limit)))};
+		auto potentialPrimes{sequence(smallestPrime, limit)};
+
+		for (auto prime : testRange) {
+			if (!includes(potentialPrimes, prime)) continue;
+
+			potentialPrimes.erase(std::remove_if(potentialPrimes.begin(),
+			                                     potentialPrimes.end(),
+			                                     [prime](auto num) {
+				                                     return prime != num && num % prime == 0;
+			                                     }),
+			                      potentialPrimes.end());
+		}
+
+		return potentialPrimes;
+	}
+
+
+} // namespace sieve
