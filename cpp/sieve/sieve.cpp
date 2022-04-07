18b22e53a89e3d89e37f2c5e29a68677b57108b1
Kyle Geske <stungeye@gmail.com>
2022-04-07 09:07:11 -0500

    Solved C++ Sieve using Cpp20.

--- cpp/sieve/sieve.cpp
+++ cpp/sieve/sieve.cpp
@@ -1,11 +1,10 @@
 #include "sieve.h"
 
 #include <algorithm>
-#include <cmath>
 
 namespace sieve {
 
-	constexpr int smallestPrime{2};
+	constexpr int smallestPrime{ 2 };
 
 	std::vector<int> sequence(int startingValue, const int endingValue) {
 		std::vector<int> v(endingValue - startingValue + 1);
@@ -14,22 +13,19 @@ namespace sieve {
 	}
 
 	bool includes(std::vector<int> haystack, const int needle) {
-		const auto it{std::ranges::find(haystack, needle)};
+		const auto it{ std::ranges::find(haystack, needle) };
 		return it != haystack.end();
 	}
 
 	std::vector<int> primes(const int limit) {
 		if (limit < smallestPrime) return {};
 
-		const auto testRange{sequence(smallestPrime, static_cast<int>(std::sqrt(limit)))};
-		auto potentialPrimes{sequence(smallestPrime, limit)};
+		auto potentialPrimes{ sequence(smallestPrime, limit) };
 
-		for (auto prime : testRange) {
-			if (!includes(potentialPrimes, prime)) continue;
-
+		for (auto prime : potentialPrimes) {
 			std::erase_if(potentialPrimes, [prime](auto num) {
 				return prime != num && num % prime == 0;
-			});
+				});
 		}
 
 		return potentialPrimes;
